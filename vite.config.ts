92b2c650ca2fc87dc0c3a4d7dd817c67e6f835ef
ratkoviclazar <lazarratkovic0@gmail.com>
2025-05-12 13:03:47 +0200

    s

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      'react': 'https://cdn.skypack.dev/react@18',
+      'react-dom': 'https://cdn.skypack.dev/react-dom@18'
+    }},
+  plugins: [react(), reactRefresh()],
   build: {
     lib: {
       entry: './src/main.js',
       formats: ['es'],
       fileName: 'my-component',
-    },
-    rollupOptions: {
-      external: [],  // Ensure React is bundled in the build
     },
   },
 });
